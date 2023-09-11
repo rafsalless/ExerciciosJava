ced7b90ecdb7eabd5b625e970fba640229ce721e
Rafsalless <rafaelasallescomer@gmail.com>
2023-09-11 17:35:14 -0300

    Exercicio de Operadores

--- src/Operadores.java
+++ src/Operadores.java
@@ -2,16 +2,14 @@ public class Operadores {
 
     public static void main( String [] args) {
 
-        int idadeJoao = 27;
-        int idadeMarcela = 18; 
+        int idadeJoao = 24;
+        int idadeMarcela = 17; 
 
-        if (idadeJoao > idadeMarcela) {
-            System.out.println("podem namorar");
-        }
-
-            else {
-            System.out.println("Nao podem namorar");
-        }
-    
+    if (idadeMarcela >= 18) { 
+        System.out.println("Podem namorar");
+        } 
+    else {
+        System.out.println("Proibido namorar");
+    }
 }
 }

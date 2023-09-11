da88149e5f4e9ed81e612bff9d30044e7b764f82
Rafsalless <rafaelasallescomer@gmail.com>
2023-09-11 18:36:25 -0300

    SmarTV com Java(metodos)

---
+++ src/Usuario.java
@@ -0,0 +1,34 @@
+public class Usuario {
+    
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        smartTv.aumentarCanal();
+         System.out.println("Canal atual: " + smartTv.canal);
+
+        smartTv.diminuirCanal();
+         System.out.println("Canal atual: " + smartTv.canal);
+         
+        smartTv.mudarCanal(15);
+        System.out.println("Canal atual: " + smartTv.canal);
+
+        System.out.println("TV Ligada?: " + smartTv.ligada);
+        System.out.println("Canal atual: " + smartTv.canal);
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        
+
+        smartTv.ligar();
+        System.out.println("TV Ligada?: " + smartTv.ligada);
+
+        smartTv.desligar();
+        System.out.println("TV Ligada?: " + smartTv.ligada);
+    }
+}
